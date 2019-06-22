dd2b38059528100ea23662079a1f0abeb00d5fc3
Oraowycliffe <oraowycliffe@gmail.com>
2019-06-22 12:51:24 +0300

    Test if the name property of the Hero constructor can be retrived

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -8,10 +8,19 @@ import static org.junit.Assert.*;
 
 public class HeroTest {
 
+    @Before
+    public void setUp() throws Exception {
+    }
+
     @Test
     public void initiates_initiatesNew_true(){
-        Hero testHero = new Hero("spiderman",20,"Web","shy");
+        Hero testHero = new Hero("Spiderman", 20, "Speed","Shy");
         assertEquals(true, testHero instanceof Hero);
+    }
+    @Test
+    public void RetrieveThename_getsName_name(){
+        Hero testHero = new Hero("Spiderman", 20, "Speed","Shy");
+        assertEquals("Spiderman", testHero.getName());
     }
 
     @After
