0789491117e84b6d7680252f0819b841e71007c4
Oraowycliffe <oraowycliffe@gmail.com>
2019-06-22 12:37:55 +0300

    Add untracked files

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,24 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hero {
+    private String mName;
+    private int mAge;
+    private String mStrength;
+    private String mWeakness;
+    private static List<Hero> instances = new ArrayList<Hero>();
+    private int mId;
+
+    public Hero(String name , int age, String strength, String weakness){
+        this.mName = name;
+        this.mAge = age;
+        this.mStrength = strength;
+        this.mWeakness = weakness;
+        this.instances.add(this);
+        this.mId = instances.size();
+
+    }
+
 }
--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,5 +1,20 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class HeroTest {
 
+    @Test
+    public void initiates_initiatesNew_true(){
+        Hero testHero = new Hero("spiderman",20,"Web","shy");
+        assertEquals(true, testHero instanceof Hero);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
 }
