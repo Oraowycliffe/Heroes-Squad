567f2147c96aed48762fa9ff3014e6a4e63065dc
Oraowycliffe <oraowycliffe@gmail.com>
2019-06-23 19:18:51 +0300

    Create the Squad class (together with its properties) then formulate the Squad constructor

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,4 +1,24 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Squad {
+    private String mName;
+    private int mSize;
+    private String mReason;
+    private static List<Squad> instances = new ArrayList<>();
+    private int mId;
+    private List <Hero> mHeroes;
+
+    public Squad (String name, int size, String reason){
+        this.mName = name;
+        this.mSize = size;
+        this.mReason = reason;
+        this.instances.add(this);
+        this.mId = instances.size();
+        this.mHeroes = new ArrayList<Hero>();
+
+    }
+
 }
