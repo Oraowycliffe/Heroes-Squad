5e5c1ac09b7c0240556460a9ce4d842831dbe776
Oraowycliffe <oraowycliffe@gmail.com>
2019-06-23 19:23:18 +0300

    Initialize the squad test and test for correct instanciation

--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -2,17 +2,25 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SquadTest {
 
-//    @Before
-//    public void setUp() throws Exception {
-//    }
-//
-//    @After
-//    public void tearDown() throws Exception {
-//    }
+    @Before
+    public void setUp() throws Exception {
+    }
+
+
+    @Test
+    public void instance_intanciatesCorrectly_true(){
+        Squad testSquad = new Squad("Furious-Four", 4, "Defence");
+        assertEquals(true, testSquad instanceof  Squad);
+
+    }
+    @After
+    public void tearDown() throws Exception {
+    }
 
 }
