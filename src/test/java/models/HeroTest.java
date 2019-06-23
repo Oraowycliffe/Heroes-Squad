1360092bd70ba9fac9c41acd86ed8a812a2cdb1e
Oraowycliffe <oraowycliffe@gmail.com>
2019-06-23 10:16:51 +0300

    Initiate test methods for the clear and all methods

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -19,25 +19,45 @@ public class HeroTest {
     }
     @Test
     public void RetrieveTheName_getsName_name(){
-        Hero testHero = new Hero("Spiderman", 20, "Speed","Shy");
-        assertEquals("Spiderman", testHero.getName());
+        Hero testHero = new Hero("Spider-man", 20, "Speed","Shy");
+        assertEquals("Spider-man", testHero.getName());
     }
     @Test
     public void RetrieveTheAge_getsAge_Age(){
-        Hero testHero = new Hero("Spiderman", 20, "Speed","Shy");
+        Hero testHero = new Hero("Spider-man", 20, "Speed","Shy");
         assertEquals(20, testHero.getAge());
     }
     @Test
     public void RetrieveTheStrength_getsStrength_Strength(){
-        Hero testHero = new Hero("Spiderman", 20, "Speed","Shy");
+        Hero testHero = new Hero("Spider-man", 20, "Speed","Shy");
         assertEquals("Speed", testHero.getStrength());
     }
     @Test
     public void RetrieveTheWeakness_getsWeakness_Weakness(){
-        Hero testHero = new Hero("Spiderman", 20, "Speed","Shy");
+        Hero testHero = new Hero("Spider-man", 20, "Speed","Shy");
         assertEquals("Shy", testHero.getWeakness());
     }
+    @Test
+    public void AddedToTheArrayList_getsAdded_true(){
 
+        Hero testHero = new Hero("Spider-man", 20, "Speed","Shy");
+        assertEquals("Shy", testHero.getWeakness());
+    }
+    @Test
+    public void all_ReturnsAllInstances_true(){
+        Hero testHero1 = new Hero("Hulk", 40, "Smashing","Afraid of ladies");
+        Hero testHero2 = new Hero ("Flash",21, "Lightning","Talks a lot");
+        assertEquals(true, Hero.all().contains(testHero1));
+        assertEquals(true, Hero.all().contains(testHero2));
+
+    }
+    @Test
+    public void clear_removesAllTheInstancesOfClassHero_0(){
+        Hero testHero1 = new Hero("Hulk", 40, "Smashing","Afraid of ladies");
+        Hero testHero2 = new Hero ("Flash",21, "Lightning","Talks a lot");
+        Hero.clear();
+        assertEquals(0, Hero.all().size());
+    }
     @After
     public void tearDown() throws Exception {
     }
